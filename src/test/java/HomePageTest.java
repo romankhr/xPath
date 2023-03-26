9286ff519e5f662d88eb6927ea0d8cfcefd95491
RomanKhrabatyn <romankhr1974@gmail.com>
2023-03-26 16:24:08 +0300

    Test xPath

--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -22,12 +22,8 @@ public class HomePageTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
-        //WebDriver driver = new ChromeDriver();
-        //
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICITLY_WAIT_SECONDS)); // 0 by default
-        //driver.manage().timeouts().implicitlyWait(IMPLICITLY_WAIT_SECONDS, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICITLY_WAIT_SECONDS));
         driver.manage().window().maximize();
-        // presentationSleep(); // For Presentation ONLY
         //
         driver.get(BASE_URL);
     }
@@ -43,8 +39,5 @@ public class HomePageTest {
         driver.findElement(By.xpath("//a[@class='nav-link dropdown-toggle' and text()='Desktops']")).click();
         driver.findElement(By.xpath("//a[@class='nav-link' and text()='Mac (1)']")).click();
         driver.findElement(By.xpath("//h4/a[text()='iMac']"));
-        //driver.findElement(By.xpath("//div/span[@class='price-new']"));
         Assert.assertEquals("111.55€", driver.findElement(By.xpath("//*[@id=\"product-list\"]/div/form/div/div[2]/div[1]/div/span[1]")).getText(), "Product iMac price is not 111.55 euros");
-        Thread.sleep(10000L);
-
 }}
